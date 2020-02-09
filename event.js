b649b6c94caa9faa95a9da92dd16cb6b1e1dcca8
ARauf Git <umiasad2017@gmail.com>
2020-02-09 11:08:21 -0500

    updated event.js

--- event.js
+++ event.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    $("#submit").on("click", function() {
+    $("#search").on("click", function() {
     
     
     
-    var city = $("#location").val();
-    var eventType = $("#interests").val();
+    var city = $("#firstInput").val();
+    var eventType = $("#secondInput").val();
     
     
     var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + city + "&keyword=" + eventType + "&apikey=5bcwmAqxVSJXdaklNuDJWfb2QRKnAXZD";
@@ -18,17 +18,23 @@ $(document).ready(function(){
     
         
         
-        for(var i = 0; i < 10; i++) {
+        for(var i = 0; i < 20; i++) {
             var newDiv = $("<div>").data("id", i);
-            $("#results").append(newDiv);
-            var name = $("<h3>").text(eventData._embedded.events[i].name);
+            newDiv.attr("class", "main-area").attr("id","mainArea");
+            $("#resultInput").append(newDiv);
+            var name = $("<h5>").text(eventData._embedded.events[i].name);
             var picLink = $("<a>").attr("href", eventData._embedded.events[i].url);
             var image = $("<img>").attr("src", eventData._embedded.events[i].images[0].url);
-            newDiv.append(name, picLink);
+            image.attr("class", "eventsImage");
+            image.attr("id", "events-image");
+            // newDiv.append(name, picLink);
+            newDiv.append(picLink);
             picLink.append(image);
             var date = $("<p>").text(eventData._embedded.events[i].dates.start.localDate);
-            var hr = $("<hr>");
-            newDiv.append(date, hr);
+            var promoters = $("<p>").text(eventData._embedded.events[i].promoters[0].name);
+            // newDiv.append(promoters);
+            
+            newDiv.append(name,promoters,date);
         }
     
     });
