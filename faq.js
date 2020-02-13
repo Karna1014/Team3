4bd1d8efbefdf780d417b563670d462d6d058425
Java K <karna.cutolo@gmail.com>
2020-02-13 12:08:44 -0500

    materialized Faq page, commented pages, deleted unnecessary faq.css and faq.js

--- faq.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-$(".faq h4").click(function(){
-    $(this).next(".questDetails").slideToggle();
-    $(this).parent().toggleClass("active");
-    $(this).parent().siblings().children(".questionDetails").slideUp;
-    $(this).parent().siblings().removeClass("active");
-});
-});
